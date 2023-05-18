01b7426016224a8bdaf697c6e4ea4255bd46759e
Jennifer Arboleda <arboleda.jen@gmail.com>
2023-05-18 17:40:17 -0500

    added Privacy Policy

---
+++ pages/privacy.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+const Privacy = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>
+        <strong>Effective Date:</strong> May 18, 2023
+      </p>
+
+      <p>
+        This Privacy Policy describes how Queerently Watching ("we," "us," or "our") collects, uses, shares, and protects the personal information obtained from users ("you" or "users") of our mobile application Queerently Watching.
+      </p>
+
+      <h2>Information We Collect</h2>
+      <p>
+        We may collect the following types of personal information:
+      </p>
+      <ul>
+        <li>Contact Information: such as your name, email address, and phone number.</li>
+        <li>Device Information: including device type, operating system version, and unique device identifiers.</li>
+        <li>Usage Information: such as your interactions with the app, pages visited, and features accessed.</li>
+        <li>Location Information: if you grant permission, we may collect your precise or approximate location.</li>
+      </ul>
+
+      <h2>How We Use Your Information</h2>
+      <p>
+        We may use the collected information for the following purposes:
+      </p>
+      <ul>
+        <li>To provide and improve our app's functionality and user experience.</li>
+        <li>To communicate with you, respond to your inquiries, and provide customer support.</li>
+        <li>To personalize your experience and deliver relevant content and advertisements.</li>
+        <li>To analyze app usage and trends, conduct research, and gather statistical information.</li>
+        <li>To detect, prevent, and address technical issues, fraud, or illegal activities.</li>
+      </ul>
+
+      <h2>Information Sharing</h2>
+      <p>
+        We may share your personal information in the following circumstances:
+      </p>
+      <ul>
+        <li>With third-party service providers who assist us in operating our app and providing services to you.</li>
+        <li>With affiliated companies, subsidiaries, or partners, for business purposes and in compliance with this Privacy Policy.</li>
+        <li>In response to legal requests, such as court orders or governmental regulations, or to protect our rights, privacy, safety, or property.</li>
+        <li>In the event of a merger, acquisition, or sale of all or a portion of our assets, your personal information may be transferred.</li>
+      </ul>
+
+      <h2>Data Retention</h2>
+      <p>
+        We will retain your personal information for as long as necessary to fulfill the purposes outlined in this Privacy Policy, unless a longer retention period is required or permitted by law.
+      </p>
+
+      <h2>Data Security</h2>
+      <p>
+        We take reasonable measures to protect the personal information we collect from unauthorized access, disclosure, alteration, or destruction. However, please note that no method of transmission over the internet or electronic storage is 100% secure.
+      </p>
+
+      <h2>Your Choices</h2>
+      <p>
+        You may choose not to provide certain personal information, although it may limit your ability to use certain features of our app. You may also have the right to access, update, or delete your personal information, subject to applicable laws.
+      </p>
+
+      <h2>Children's Privacy</h2>
+      <p>
+        Our app is not intended for individuals under the age of [insert age]. We do not knowingly collect personal information from children. If you believe we have inadvertently collected information from a child, please contact us immediately.
+      </p>
+
+      <h2>Changes to this Privacy Policy</h2>
+      <p>
+        We reserve the right to update or modify this Privacy Policy from time to time. Any changes will be effective upon posting the updated Privacy Policy. We encourage you to review this Privacy Policy periodically.
+      </p>
+
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions or concerns regarding this Privacy Policy or our privacy practices, please contact us at [contact email or address].
+      </p>
+    </div>
+  );
+};
+
+export default Privacy;
