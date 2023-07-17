ebc1663b63098b2c089706700203eee8c5aef003
Sarah Amann <sarah@Sarahs-MacBook-Pro.local>
2023-07-17 16:49:27 -0500

    fixed the auth flicker

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,18 @@
 import HomeSignedOut from "component/components/HomeSignedOut";
-import HomeSignedIn from "component/components/HomeSignedIn";
-import { useSession } from "@supabase/auth-helpers-react"
 import { useRouter } from "next/router";
-import Dashboard from "./dashboard";
+import { useSessionContext } from '@supabase/auth-helpers-react';
 
 export default function Home() {
-  const  session  = useSession();
   const router = useRouter();
-  console.log("session here", session);
-
-  // while this sort of works because they share the same route, what I would probably do is make the 
-  // "HomeSignedIn" a "/dashboard" and make it a protected route. That way it won't have the weird flashing
-  // that happens for an instant when it does the session check after the component has mounted.
-
-  // return (
-  //   <>
-  //     <div>
-  //       {session ? 
-  //       <Dashboard /> : <HomeSignedOut /> }
-  //     </div>
-  //   </>
-  // );
+  const { isLoading, session, error } = useSessionContext();
 
 
-  if (session) {
+  // make this a cute loading component if ya wanna 😊
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (session && isLoading == false) {
     router.push("/dashboard");
     return null;
   }
